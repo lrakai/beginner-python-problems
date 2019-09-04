9a5daf91e11354b10b5946c3979f691a033bd493
Logan Rakai <lmrakai@gmail.com>
2019-09-04 16:55:37 -0600

    Add concealed test

--- test/test_challenges.py
+++ test/test_challenges.py
@@ -2,7 +2,7 @@ import sys
 import os
 import unittest
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../solutions")
 from challenge_one import odds
 
 
@@ -12,6 +12,8 @@ class TestChallenge(unittest.TestCase):
         self.assertEqual(odds([0, 1, 2, 3, 4, 5]), [1, 3, 5])
         self.assertEqual(odds(['Matt', 'Andy', 'Tom', 'Jeremy']), 
                               ['Andy', 'Jeremy'])
+        self.assertEqual(odds(['This', 'one', 'is', 'hidden', '.']), 
+                              ['one', 'hidden'])
 
 
 if __name__ == '__main__':
